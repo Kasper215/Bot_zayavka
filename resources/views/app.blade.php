40f923c98c3564fc920db6f55ef2d317fbc6854d
Kasper <daniilnazarenko313@mail.ru>
2026-03-30 21:58:33 +0300

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
         @inertiaHead
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="manifest" href="/manifest.json">
-        <link rel="apple-touch-icon" href="/pwa-icon.png">
+        <link rel="apple-touch-icon" href="/launchericon-512x512.png">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="theme-color" content="#0f172a">
